f15eba431ba4674ba0c5c20990f75c6bf1b33e7e
Teroiyet <44843491+Teroiyet@users.noreply.github.com>
2019-05-03 17:10:02 +0200

    Update and rename ServerClientThread.java to ClientChat.java

---
+++ ClientChat.java
@@ -0,0 +1,76 @@
+package server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+public class ClientChat implements Runnable{
+    public JTextField tx;
+    public JTextArea ta;
+    public String login="";
+    BufferedWriter os;
+    BufferedReader is;
+   
+    public ClientChat(String l){
+        login=l;        
+        
+        JFrame f=new JFrame("G-INF1 : IPSAS");
+        f.setSize(400,400);        
+        
+        JPanel p1=new JPanel();
+        p1.setLayout(new BorderLayout());
+            
+        JPanel p2=new JPanel();
+        p2.setLayout(new BorderLayout());        
+        
+        tx=new JTextField();
+        p1.add(tx, BorderLayout.CENTER);
+        
+        JButton b1=new JButton("Send");
+        p1.add(b1, BorderLayout.EAST); 
+        
+        ta=new JTextArea();
+        p2.add(ta, BorderLayout.CENTER);
+        p2.add(p1, BorderLayout.SOUTH);
+        
+        f.setContentPane(p2);
+           
+ 
+           
+        try{
+                 Socket socketClient= new Socket("localhost",8888);
+                  os= new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+                 
+                  is =new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+                 
+ 
+			
+		    		
+        }catch(Exception e){e.printStackTrace();}
+    
+    
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ev){
+                String s=login+" : "+tx.getText();  
+                tx.setText("");
+                try{
+                    os.write(s);
+                    os.write("\r\n");
+                    os.flush(); 
+                    }catch(Exception e){e.printStackTrace();}
+            }
+          }
+        );       
+        f.setVisible(true);    
+    }
+    public void run(){
+             try{
+                String serverMsg=""; 
+                while((serverMsg = is.readLine()) != null){
+                    System.out.println("from server: " + serverMsg);
+                    ta.append(serverMsg+"\n");
+                }
+        }catch(Exception e){e.printStackTrace();}   
+    }
+}
