3aeb18088359c0cbd9558a165bf981850e1af94c
Teroiyet <44843491+Teroiyet@users.noreply.github.com>
2019-05-03 17:13:45 +0200

    Update StartClient.java

--- StartClient.java
+++ StartClient.java
@@ -6,9 +6,10 @@ public class StartClient{
     
     public static void main(String [] args){
         	   try{
-        	        System.out.println("Hello ");  
+        	        
         	        String name = JOptionPane.showInputDialog(null,"Enter your name :", "Username",
         	                JOptionPane.PLAIN_MESSAGE);
+			   System.out.println("Hello "+name);  
                         ClientChat c=new ClientChat(name);   
                         Thread t1=new Thread(c);
                         t1.start();
