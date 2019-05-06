7ed75ec71c06bdfd8056835208857c75803854e8
Teroiyet <44843491+Teroiyet@users.noreply.github.com>
2019-05-06 14:34:38 +0200

    Update StartClient.java

--- StartClient.java
+++ StartClient.java
@@ -1,3 +1,16 @@
+                        /******************************************************
+                         ******************************************************
+                          * Developed By :                                   *
+                          *       Aymen Teroiyet           ||  IPSAS   ||    * 
+                          *                             ||  G.inf1  ||       *    
+                          *            Omar Masmoudi                         * 
+                          *                                                  *
+                          *                 Abdallah Trabellsi               *
+                          *                                        2018-2019 *
+                         ******************************************************
+                         ******************************************************/
+
+
 package server;
 
 import javax.swing.JOptionPane;
@@ -6,10 +19,10 @@ public class StartClient{
     
     public static void main(String [] args){
         	   try{
-        	        
+        	          
         	        String name = JOptionPane.showInputDialog(null,"Enter your name :", "Username",
         	                JOptionPane.PLAIN_MESSAGE);
-			   System.out.println("Hello "+name);  
+        	        System.out.println("Hello "+name);
                         ClientChat c=new ClientChat(name);   
                         Thread t1=new Thread(c);
                         t1.start();
