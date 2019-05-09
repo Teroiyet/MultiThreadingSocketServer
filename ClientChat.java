067070758ece250358875bf91013b31959b51103
Teroiyet <44843491+Teroiyet@users.noreply.github.com>
2019-05-09 02:47:46 +0200

    Update ClientChat.java

--- ClientChat.java
+++ ClientChat.java
@@ -2,7 +2,7 @@
                          ******************************************************
                           * Developed By :                                   *
                           *       Aymen Teroiyet           ||  IPSAS   ||    * 
-                          *                             ||  G.inf1  ||       *    
+                          *                                ||  G.inf1  ||    *    
                           *            Omar Masmoudi                         * 
                           *                                                  *
                           *                 Abdallah Trabellsi               *
@@ -26,38 +26,38 @@ public class ClientChat implements Runnable{
     BufferedReader is;
    
     public ClientChat(String l){
-        login=l;        
+        login=l;        // username
         
-        JFrame f=new JFrame("G-INF1 : IPSAS");
-        f.setSize(400,400);        
-        
-        JPanel p1=new JPanel();
-        p1.setLayout(new BorderLayout());
-            
-        JPanel p2=new JPanel();
-        p2.setLayout(new BorderLayout());        
-        
-        tx=new JTextField();
-        p1.add(tx, BorderLayout.CENTER);
-        
-        JButton b1=new JButton("Send");
-        p1.add(b1, BorderLayout.EAST); 
-        
-        ta=new JTextArea();
-        p2.add(ta, BorderLayout.CENTER);
-        p2.add(p1, BorderLayout.SOUTH);
-        
-        f.setContentPane(p2);
+        JFrame f=new JFrame(login);        //
+        f.setSize(400,400);                //
+                                           //
+        JPanel p1=new JPanel();            //
+        p1.setLayout(new BorderLayout());  //
+                                           //
+        JPanel p2=new JPanel();            // 
+        p2.setLayout(new BorderLayout());  //          
+                                           //
+        tx=new JTextField();               //   create a graphical interface for the ChatApp
+        p1.add(tx, BorderLayout.CENTER);   //
+                                           //
+        JButton b1=new JButton("Send");    //
+        p1.add(b1, BorderLayout.EAST);     //
+                                           //
+        ta=new JTextArea();                //
+        p2.add(ta, BorderLayout.CENTER);   //
+        p2.add(p1, BorderLayout.SOUTH);    //
+                                           //
+        f.setContentPane(p2);              //
            
  
            
         try{
-                 Socket socketClient= new Socket("localhost",8888);
-                  os= new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+                 Socket socketClient= new Socket("localhost",8888);//create an object of type Socket establish a connection to the server on the server's IP(in this case we use the localhost) and server's PORT (8888)
+                  os= new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));// bufferedWriter for the write(Mic) of string's data
                  
-                  is =new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+                  is =new BufferedReader(new InputStreamReader(socketClient.getInputStream()));// bufferedReader for the read(SPEAKER) of string's data
                  
-                  os.write(login);
+                  os.write(login);// send the username to add it into the users's vector 
                   os.write("\r\n");
                   os.flush();
  
@@ -66,16 +66,16 @@ public class ClientChat implements Runnable{
         }catch(Exception e){e.printStackTrace();}
     
     
-        b1.addActionListener(new ActionListener(){
+        b1.addActionListener(new ActionListener(){        // the button event
             public void actionPerformed(ActionEvent ev){
                 String s1=login+" : ";
                 String s2=tx.getText();
                 tx.setText("");
                 try{
-                    os.write(s1);
+                    os.write(s1);// send the username to append it to the chat interface
                     os.write("\r\n");
                     os.flush(); 
-                    os.write(s2);
+                    os.write(s2); // send the message
                     os.write("\r\n");
                     os.flush();
                     }
@@ -92,9 +92,9 @@ public class ClientChat implements Runnable{
     public void run(){
              try{
                 String serverMsg=""; 
-                while((serverMsg = is.readLine()) != null&&serverMsg.charAt(0)!=' '){
+                while((serverMsg = is.readLine())/*read the msg of other client switching by the server*/ != null&&serverMsg.charAt(0)!=' '){
                     System.out.println("from server: " + serverMsg);
-                    ta.append(serverMsg+"\n");
+                    ta.append(serverMsg+"\n");// append the msg to the chat interface
                 }
                 
         }catch(Exception e){e.printStackTrace();}   
